b07177b07863dc1f967b902cffb161d9a0884eaa
jakimowb <benjamin.jakimow@geo.hu-berlin.de>
2023-06-24 00:01:46 +0200

    testing

--- runtests.bat
+++ runtests.bat
@@ -1,9 +1,4 @@
 
 ::mkdir test-reports
 set CI=True
-@echo off
-::call :sub > test-report.txt
-exit /b
-
-:sub
-python -m nose2 --verbose discover tests "test_*.py"
+pytest
--- runtests.sh
+++ runtests.sh
@@ -3,5 +3,9 @@ export QT_QPA_PLATFORM=offscreen
 export CI=True
 rm -Rf test-outputs
 rm -Rf test-reports
-pytest --no-cov-on-fapyil --cov-config=.coveragec
-coverage-badge -o coverage.svg
+export PYTHONPATH="$(pwd);$(pwd)/tests/qgispluginsupport$PYTHONPATH"
+echo $PYTHONPATH
+
+# export PYTHONPATH="$(pwd)":tests/qgispluginsupport
+pytest
+#coverage-badge -o coverage.svg
