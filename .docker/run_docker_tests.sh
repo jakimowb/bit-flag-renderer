a6a6b0b50f136e0c320cf9b643a3bca3e7f8a799
jakimowb <benjamin.jakimow@geo.hu-berlin.de>
2023-06-24 00:05:42 +0200

    added docker tests

---
+++ .docker/run_docker_tests.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+#***************************************************************************
+#***************************************************************************
+#
+#***************************************************************************
+#*                                                                         *
+#*   This program is free software; you can redistribute it and/or modify  *
+#*   it under the terms of the GNU General Public License as published by  *
+#*   the Free Software Foundation; either version 2 of the License, or     *
+#*   (at your option) any later version.                                   *
+#*                                                                         *
+#***************************************************************************
+
+set -e
+
+pushd /usr/src
+DEFAULT_PARAMS='-x -v'
+cd /usr/src
+export QT_QPA_PLATFORM=offscreen
+export CI=True
+export PYTHONPATH="${PYTHONPATH}:$(pwd):$(pwd)/tests/qgispluginsupport"
+python3 scripts/compile_resourcefiles.py
+pytest
+popd
